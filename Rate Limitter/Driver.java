ca3656a19769233e09dfe3d36176f9e2e3976253
Riddhi Dutta <rite2riddhi@gmail.com>
2022-08-08 00:34:36 +0530

    Update Driver.java

--- Rate Limitter/Driver.java
+++ Rate Limitter/Driver.java
@@ -4,16 +4,29 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
+//		RateLimitterService rateLimitterService = new RateLimitterService();
+//		for(int i = 1 ; i <= 2 ; i ++)
+//			rateLimitterService.allowAccess("123", "Service1", 3);
+//		rateLimitterService.allowAccess("456", "Service1", 3);
+//		
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		rateLimitterService.allowAccess("123", "Service1", 3);
+		
 		RateLimitterService rateLimitterService = new RateLimitterService();
 		for(int i = 1 ; i <= 2 ; i ++)
-			rateLimitterService.allowAccess("123", "Service1", 3);
+			rateLimitterService.allowAccess("123", "Service2", 2);
+		rateLimitterService.allowAccess("456", "Service2", 2);
 		
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		rateLimitterService.allowAccess("123", "Service1", 3);
+		rateLimitterService.allowAccess("123", "Service2", 2);
 	}
 
 }
